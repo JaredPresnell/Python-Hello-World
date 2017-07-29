22b51065da50d4e18b54f39afed60d3d58348d1d
koza <kevinkoza@gmail.com>
2017-07-29 12:33:08 -0700

    loader

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print 'hello'
+from main2	import Kevin as k
+from time import sleep
+import sys
+
+def main():
+	load_indefinitely()
+
+def load_indefinitely():
+	waittime = 1
+	while True:
+		sys.stdout.write('\r|')
+		sleep(waittime)
+		sys.stdout.flush()
+		sys.stdout.write('\r/')
+		sleep(waittime)
+		sys.stdout.flush()
+		sys.stdout.write('\r-')
+		sleep(waittime)
+		sys.stdout.flush()
+		sys.stdout.write('\r\\')
+		sleep(waittime)
+		sys.stdout.flush()
+		sys.stdout.write('\r|')
+		sleep(waittime)
+		sys.stdout.flush()
+		sys.stdout.write('\r/')
+		sleep(waittime)
+		sys.stdout.flush()
+		sys.stdout.write('\r-')
+		sleep(waittime)
+		sys.stdout.flush()
+		sys.stdout.write('\r\\')
+		sleep(waittime)
+		sys.stdout.flush()
+
+
+if __name__ == "__main__":
+	main()
